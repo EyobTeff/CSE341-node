c860083360b25b5665595b6452504330d1d4f4dc
EyobTeff <eyobteff@gmail.com>
2025-11-03 02:06:30 -0700

    Add root route

--- contacts-api/server.js
+++ contacts-api/server.js
@@ -5,6 +5,12 @@ const app = express();
 const contactsRoutes = require('./routes/contacts');
 
 app.use(express.json());
+
+// Root route
+app.get('/', (req, res) => {
+  res.send('Contacts API - Use /contacts to get all contacts');
+});
+
 app.use('/contacts', contactsRoutes);
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server running on port ${process.env.PORT || 3000}`);
